89851fd7df985d5af8bcf332afc434ab4977dd85
daheige <zhuwei313@hotmail.com>
2022-01-15 18:00:10 +0800

    wg test

--- waitgroup/waitgroup_test.go
+++ waitgroup/waitgroup_test.go
@@ -18,9 +18,27 @@ func TestWrapper(t *testing.T) {
 	wg.Wrap(func() {
 		log.Println("1111")
 	})
+
+	for i := 0; i < 10*10000; i++ {
+		// The method of copying is used here to avoid the i
+		// in the wrap func being the same variable
+		index := i
+		wg.Wrap(func() {
+			log.Printf("current index: %d\n", index)
+		})
+	}
+
 	wg.WrapWithRecover(func() {
-		log.Println(2222)
 		panic("mock panic test")
 	})
+
 	wg.Wait()
 }
+
+/*
+2022/01/15 17:58:32 current index: 99998
+2022/01/15 17:58:32 current index: 99999
+2022/01/15 17:58:32 exec recover:mock panic test
+--- PASS: TestWrapper (4.66s)
+PASS
+*/
